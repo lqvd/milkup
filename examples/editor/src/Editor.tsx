8c78a70ce7ee6c437ce078526052be52392d856a
Liam Doan <lqd22@ic.ac.uk>
2025-01-20 17:00:40 +0000

    fix: correct issues with placeholder css

--- examples/editor/src/Editor.tsx
+++ examples/editor/src/Editor.tsx
@@ -4,8 +4,14 @@ import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
 import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
 import './Editor.css';
 
+const theme = {
+  placeholder: "editor-placeholder",
+  paragraph: "editor-paragraph",
+}
+
 const initialConfig = {
   namespace: 'Milkup',
+  theme,
   onError: (error: Error) => console.error(error),
 };
 
@@ -13,11 +19,13 @@ export default function Milkup() {
   return (
     <LexicalComposer initialConfig={initialConfig}>
       <div className="editor-container">
-        <RichTextPlugin
-          contentEditable={<ContentEditable className="editor-input" />}
-          placeholder={<div className="editor-placeholder"></div>}
-          ErrorBoundary={LexicalErrorBoundary}
-        />
+        <div className="editor-inner">
+          <RichTextPlugin
+            contentEditable={<ContentEditable className="editor-input" />}
+            placeholder={<div className="editor-placeholder">Placeholder</div>}
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+        </div>
       </div>
     </LexicalComposer>
   );
