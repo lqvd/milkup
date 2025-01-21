0d259054c1e9902fdc8b6da027a152c3c84f016a
Owen Li <owen.li22@imperial.ac.uk>
2025-01-21 15:51:35 +0000

    feat: added toolbar and styling from lexical example.

--- examples/editor/src/Editor.tsx
+++ examples/editor/src/Editor.tsx
@@ -3,7 +3,7 @@ import { ContentEditable } from '@lexical/react/LexicalContentEditable';
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
 import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
 import { MarkdownShortcutPlugin } from '@lexical/react/LexicalMarkdownShortcutPlugin';
-import './Editor.css';
+// import './Editor.css';
 import { TRANSFORMERS } from './plugins/transformers';
 import { HorizontalRuleNode } from '@lexical/react/LexicalHorizontalRuleNode';
 
@@ -21,9 +21,78 @@ import { EquationNode } from '../../../packages/milkup-equations/src/EquationNod
 import { SharedHistoryContext } from './plugins/SharedHistoryContext';
 import TreeViewPlugin from './plugins/TreeViewPlugin';
 
+import "./lexical-styling.css"
+import ToolbarPlugin from './plugins/toolbar-plugin';
+
 const theme = {
-  placeholder: "editor-placeholder",
-  paragraph: "editor-paragraph",
+  ltr: 'ltr',
+  rtl: 'rtl',
+  paragraph: 'editor-paragraph',
+  quote: 'editor-quote',
+  heading: {
+    h1: 'editor-heading-h1',
+    h2: 'editor-heading-h2',
+    h3: 'editor-heading-h3',
+    h4: 'editor-heading-h4',
+    h5: 'editor-heading-h5',
+    h6: 'editor-heading-h6',
+  },
+  list: {
+    nested: {
+      listitem: 'editor-nested-listitem',
+    },
+    ol: 'editor-list-ol',
+    ul: 'editor-list-ul',
+    listitem: 'editor-listItem',
+    listitemChecked: 'editor-listItemChecked',
+    listitemUnchecked: 'editor-listItemUnchecked',
+  },
+  hashtag: 'editor-hashtag',
+  image: 'editor-image',
+  link: 'editor-link',
+  text: {
+    bold: 'editor-textBold',
+    code: 'editor-textCode',
+    italic: 'editor-textItalic',
+    strikethrough: 'editor-textStrikethrough',
+    subscript: 'editor-textSubscript',
+    superscript: 'editor-textSuperscript',
+    underline: 'editor-textUnderline',
+    underlineStrikethrough: 'editor-textUnderlineStrikethrough',
+  },
+  code: 'editor-code',
+  codeHighlight: {
+    atrule: 'editor-tokenAttr',
+    attr: 'editor-tokenAttr',
+    boolean: 'editor-tokenProperty',
+    builtin: 'editor-tokenSelector',
+    cdata: 'editor-tokenComment',
+    char: 'editor-tokenSelector',
+    class: 'editor-tokenFunction',
+    'class-name': 'editor-tokenFunction',
+    comment: 'editor-tokenComment',
+    constant: 'editor-tokenProperty',
+    deleted: 'editor-tokenProperty',
+    doctype: 'editor-tokenComment',
+    entity: 'editor-tokenOperator',
+    function: 'editor-tokenFunction',
+    important: 'editor-tokenVariable',
+    inserted: 'editor-tokenSelector',
+    keyword: 'editor-tokenAttr',
+    namespace: 'editor-tokenVariable',
+    number: 'editor-tokenProperty',
+    operator: 'editor-tokenOperator',
+    prolog: 'editor-tokenComment',
+    property: 'editor-tokenProperty',
+    punctuation: 'editor-tokenPunctuation',
+    regex: 'editor-tokenVariable',
+    selector: 'editor-tokenSelector',
+    string: 'editor-tokenSelector',
+    symbol: 'editor-tokenProperty',
+    tag: 'editor-tokenProperty',
+    url: 'editor-tokenOperator',
+    variable: 'editor-tokenVariable',
+  },
 }
 
 const initialConfig = {
@@ -52,6 +121,7 @@ export default function Milkup() {
     <LexicalComposer initialConfig={initialConfig}>
       <SharedHistoryContext>
         <div className="editor-container">
+          <ToolbarPlugin />
           <div className="editor-inner">
             <RichTextPlugin
               contentEditable={<ContentEditable className="editor-input" />}
