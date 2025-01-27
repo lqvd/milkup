71df6bb85e952aa218592f599b031db9c8c65763
Theo Akande <ta722@ic.ac.uk>
2025-01-27 15:03:52 +0000

    feat: milkup audio component

---
+++ packages/milkup-audio/src/AudioComponent.tsx
@@ -0,0 +1,37 @@
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import useLexicalEditable from "@lexical/react/useLexicalEditable";
+import { NodeKey } from "lexical";
+import { useCallback, useRef, useState } from "react";
+
+type AudioComponentProps = {
+    source: string;
+    nodeKey: NodeKey;
+}
+
+export default function AudioComponent({
+  source,
+  nodeKey,
+}: AudioComponentProps): JSX.Element {
+  const [editor] = useLexicalComposerContext();
+  const isEditable = useLexicalEditable();
+  const [audioValue, setAudioValue] = useState(source);
+
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const handleEnded = useCallback(() => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = 0;
+      audioRef.current.pause();
+    }
+  }, [audioRef]);
+
+  return (
+    <div className="audio-action">
+      <audio 
+        ref={audioRef}
+        controls 
+        src={source} 
+        onEnded={handleEnded}/>
+    </div>
+  );
+}
