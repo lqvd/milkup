a59cc65f86e35c12fe08936403b35afeeff45d38
Theo Akande <ta722@ic.ac.uk>
2025-02-05 13:22:45 +0000

    fix: correct regex match for inline audio

--- packages/milkup-audio/src/transformers.tsx
+++ packages/milkup-audio/src/transformers.tsx
@@ -2,8 +2,8 @@ import { ElementTransformer } from "@lexical/markdown";
 import { $createAudioNode, $isAudioNode, AudioNode } from "./AudioNode";
 import { ElementNode } from "lexical";
 
-const AUDIO_MD_REGEX = /\[AUDIO_EMBED\]\((.*)\)/;
-const AUDIO_LINK_REGEX = /(.*)\.(mp3|wav|flac|aac|ogg|m4a|wma)/;
+const AUDIO_MD_REGEX = /(.*)\[AUDIO_EMBED\]\((.*)\)/;
+const AUDIO_LINK_REGEX = /((.*)\s)?(\S*)\.(mp3|wav|flac|aac|ogg|m4a|wma)/;
 const AUDIO_REGEX = new RegExp(
   AUDIO_MD_REGEX.source + "|" + AUDIO_LINK_REGEX.source,
 );
@@ -18,9 +18,16 @@ export const AUDIO_EMBED: ElementTransformer = {
   },
   regExp: AUDIO_REGEX,
   replace: (parentNode: ElementNode, _1, match, isImport: boolean) => {
-    const newNode = $createAudioNode(match[isImport ? 1 : 0]);
-    if (parentNode.getNextSibling() != null || isImport) {
+    console.log(match);
+    console.log(parentNode);
+
+    const link = !isImport ? match[5] + "." + match[6] : match[2];
+
+    const newNode = $createAudioNode(link);
+    if (parentNode.getNextSibling() != null) {
       parentNode.replace(newNode);
+    } else if (isImport) {
+      parentNode.insertBefore(newNode);
     } else {
       parentNode.insertBefore(newNode);
       parentNode.selectNext();
